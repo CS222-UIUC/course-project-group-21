7d790a21fa02cd612b3adc260dae59de73b05d24
Yiphen-Ori <yflin68@gmail.com>
2022-10-14 19:33:43 -0500

    Revert "Made a basic React app"
    This reverts commit 58e86ae1cc21ee32afbc7db7d80c719ab97c17bc.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Basic react app.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          It's empty I know.
-        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
