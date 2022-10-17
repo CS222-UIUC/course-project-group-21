2585a16e9420d2ab3b332d663c303ba9844b810c
Yiphen-Ori <yflin68@gmail.com>
2022-10-16 21:27:31 -0500

    Added a basic weather API to the react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 
+// For APIs
+import 'semantic-ui-css/semantic.min.css';
+import { Dimmer, Loader } from 'semantic-ui-react';
+import React, { useEffect, useState } from "react";
+import Weather from './components/weather';
+
 function App() {
+
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+    fetchData();
+  }, [lat,long])
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <p>
           Basic react app.
         </p>
         <p>
           It's empty I know.
         </p>
-      </header>
+      </header> */}
+
+      {(typeof data.main != 'undefined') ? (
+        <Weather weatherData={data}/>
+      ): (
+        <div>
+          <Dimmer active>
+            <Loader>Loading..</Loader>
+          </Dimmer>
+      </div>
+      )}
+      
     </div>
   );
 }
