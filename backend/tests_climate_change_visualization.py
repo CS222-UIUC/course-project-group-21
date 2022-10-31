44555942d7cbed066f6f5b77e973c201519a5a1c
Aashi <at33@illinois.edu>
2022-10-31 18:46:12 -0500

    readding climation change visualization and tests

---
+++ backend/tests_climate_change_visualization.py
@@ -0,0 +1,7 @@
+import pytest
+import climate_change_visualization
+
+def test_climate_change_visualization():
+    climate_change_visualization.climate_change_visualization()
+    assert(1)
+    assert(1)
